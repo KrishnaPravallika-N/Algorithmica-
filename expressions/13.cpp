67a56a2c534812a45a32e6750a1667b9bf8d308a
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2022-12-07 14:26:58 +0530

    update

--- expressions/13.cpp
+++ expressions/13.cpp
@@ -3,12 +3,20 @@
 using namespace std;
 
 int main() {
-    int n,sum = 0,temp;
+    int n,sum = 0,t1,t2,t3,t4;
     cin>>n;
-    while(n>0){
-        temp = n%10;
-        sum = sum + temp;
-        n = n/10;
-    }
+    t1 = n%10;
+    sum = sum + t1;
+    n = n/10;
+    t2 = n%10;
+    sum = sum + t2;
+    n = n/10;
+    t3 = n%10;
+    sum = sum + t3;
+    n = n/10;
+    t4 = n%10;
+    sum = sum + t4;
+    n = n/10;
+    
     cout<<sum;
 }
