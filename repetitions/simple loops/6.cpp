c3cf2a21a5caebea7da26357faa89a452c8a8249
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2022-12-12 03:12:19 -0600

    update

--- repetitions/simple loops/6.cpp
+++ repetitions/simple loops/6.cpp
@@ -25,16 +25,16 @@ int main(){
 	else if(k<0){
 		for(int i=0;i<s.length();i++){
 		if(s[i] >= 'a' && s[i] <= 'z'){
-			s[i] = s[i] - k;
+			s[i] = s[i] + k;
 			if(s[i] < 'z'){
-				s[i] = s[i] +'z' - 'a' + 1;
+				s[i] = s[i] -'z' + 'a' - 1;
 			}
 		}
 		
 		else if (s[i] >= 'A' && s[i] <= 'Z'){
             s[i] = s[i] - k;
             if (s[i] > 'Z'){
-               s[i] = s[i] + 'Z' - 'A' + 1;
+               s[i] = s[i] - 'Z' + 'A' - 1;
             }
          }
 		}
--- repetitions/simple loops/7.cpp
+++ repetitions/simple loops/7.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main(){
 	int x;
-	float guess,avg,power,t;
+	float guess,avg;
+	double power,t;
 	cin>>x;
+	guess = x/2;
 	t = abs(x - (guess*guess));
 	power = pow(10,-12);
 	while(t>power){
-		guess = x/2;
 		guess = (guess + (x/guess))/2;
 	}
 	cout<<guess;
