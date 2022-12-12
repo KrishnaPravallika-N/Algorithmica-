f4867f197ffbe94362eaa42ede152e2a53e039a6
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2022-12-12 15:50:38 +0530

    update

--- repetitions/simple loops/7.cpp
+++ repetitions/simple loops/7.cpp
@@ -3,13 +3,12 @@ using namespace std;
 int main(){
 	int x;
 	float guess,avg;
-	double power,t;
+	double power;
 	cin>>x;
 	guess = x/2;
-	t = abs(x - (guess*guess));
-	power = pow(10,-12);
-	while(t>power){
-		guess = (guess + (x/guess))/2;
+	power = pow(10,-8);
+	while(abs(x - (guess*guess))>=power){
+		guess = (guess + int(x)/guess)/2.0;
 	}
 	cout<<guess;
 }
