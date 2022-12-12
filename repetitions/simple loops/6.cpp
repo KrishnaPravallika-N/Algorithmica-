00ea157951dc896816daea74966fc4ff5c18403e
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2022-12-12 16:20:53 +0530

    update

--- repetitions/simple loops/6.cpp
+++ repetitions/simple loops/6.cpp
@@ -1,43 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	string s;
-	int temp,k,t1,t2;
-	cin>>k>>s;
-	if(k>0){
-		for(int i=0;i<s.length();i++){
+	string s,result = "";
+	int k,pos;
+	cin>>s>>k;
+	for(int i=0;i<s.length();i++){
 		if(s[i] >= 'a' && s[i] <= 'z'){
-			s[i] = s[i] + k;
-			if(s[i] > 'z'){
-				s[i] = s[i] - 'z' + 'a' - 1;
-			}
+			pos = int(s[i]) - 'a';
+			pos = (pos+k)%26;
+			result += char('a' + pos);
 		}
-		
-		else if (s[i] >= 'A' && s[i] <= 'Z'){
-            s[i] = s[i] + k;
-            if (s[i] > 'Z'){
-               s[i] = s[i] - 'Z' + 'A' - 1;
-            }
-         }
-      }
-      cout<<s;
+		else if(s[i] >= 'A' && s[i] <= 'Z'){
+			pos = int(s[i]) - 'A';
+			pos = (pos+k)%26;
+			result += char('A' + pos);
+		}
 	}
-	else if(k<0){
-		for(int i=0;i<s.length();i++){
-		if(s[i] >= 'a' && s[i] <= 'z'){
-			s[i] = s[i] + k;
-			if(s[i] < 'z'){
-				s[i] = s[i] -'z' + 'a' - 1;
-			}
-		}
-		
-		else if (s[i] >= 'A' && s[i] <= 'Z'){
-            s[i] = s[i] - k;
-            if (s[i] > 'Z'){
-               s[i] = s[i] - 'Z' + 'A' - 1;
-            }
-         }
-		}
-		cout<<s;
-	}
+	cout<<result;
 }
