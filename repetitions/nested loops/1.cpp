74354235cec2a7d21c57893ec7be357952772ed6
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2022-12-14 16:12:00 +0530

    1

--- repetitions/nested loops/1.cpp
+++ repetitions/nested loops/1.cpp
@@ -3,10 +3,9 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-	for(int i=n;i>0;i--){
-		for(int j=0;j<=n;j++){
-			if(j>=i) cout<<"* ";
-			else cout<<" ";
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout<<"* ";
 		}
 		cout<<"\n";
 	}
