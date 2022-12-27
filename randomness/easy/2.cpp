6b236970b4c327a2f6c4a0ea40d49cfa0fded0a4
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2022-12-27 15:21:23 +0530

    update

--- randomness/easy/2.cpp
+++ randomness/easy/2.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int diceGame(){
 	int i,t,count = 0;
 	srand(time(0));
-	t = rand()%6 + 1;
 	for(int i=0;i<10;i++){
+		t = rand()%6 + 1;
 		if(t == 6)count++;
 	}
 	return count;
