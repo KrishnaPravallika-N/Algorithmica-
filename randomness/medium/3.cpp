6111b77657a56a61b1466ece180071a4f386902b
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2022-12-30 15:00:47 +0530

    update

--- randomness/medium/3.cpp
+++ randomness/medium/3.cpp
@@ -2,14 +2,24 @@
 #include<cstdlib>
 using namespace std;
 int getNumOfFlips(){
-	int r = rand() % 2;
 	int nflips = 0;
 	int prev = -1;
 	while(true){
+		srand(time(0));
 		int current = rand() % 2;
-		prev++;
+		nflips++;
 		if((prev == 1 && current == 1) || (prev == 0 && current == 0)) return nflips;
 		prev == current;
 	}
 	return 0;
+}
+int main(){
+	int min=1000000000,max=0,avg=0;
+	for(int i=1;i<=10;i++){
+		int x = getNumOfFlips();
+		if(x<min)min = x;
+		if(x>max)max = x;
+		avg = (avg+x)/2;
+	}
+	cout<<min<<" "<<max<<" "<<avg;
 }
