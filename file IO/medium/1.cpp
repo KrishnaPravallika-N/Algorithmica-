39d70fa3814376467e2ff5077e5fc0614fcb3152
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2023-01-10 21:19:31 +0530

    update

--- file IO/medium/1.cpp
+++ file IO/medium/1.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 void missingComments(string in_name) {
 	ifstream fin(in_name);
-	ofstream fout(out_name);
+	ofstream fout(in_name);
 	string line;
 	string prev = " ";
 	int lineNum = 0;
 	while(getline(fin, line)) {
 		lineNum++;
-		if(line.startsWith("def") && prev[0] != '#'){
-			string fname = line.substring(4,line.index('('));
-			fout<<fname + ":" +lineNum;	
+		if((line.rfind("def",0) == 0) && prev[0] != '#'){
+			string fname = line.substr(4,line.find('('));
+			fout<<fname<<":"<<lineNum;	
 		}
 		if(line.length() == 0)prev = "";
 		prev = line;
@@ -20,7 +20,7 @@ void missingComments(string in_name) {
 	fin.close();
 }
 int main() {
-	string in_name, out_name;
+	string in_name;
 	cin >> in_name;
 	missingComments(in_name);
 }
