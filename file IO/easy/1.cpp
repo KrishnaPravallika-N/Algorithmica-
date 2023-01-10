98b57a94c98c577e4f02823ca611f8d726604e00
KrishnaPravallika-N <119158891+KrishnaPravallika-N@users.noreply.github.com>
2023-01-10 21:07:16 +0530

    initial

--- file IO/easy/1.cpp
+++ file IO/easy/1.cpp
@@ -1,15 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <fstream>
 using namespace std;
 
-void removeComments(string in_name, string out_name) {
+void readWrite(string in_name, string out_name) {
 	ifstream fin(in_name);
 	ofstream fout(out_name);
 	string line;
 	while(getline(fin, line)) {
-		int pos = line.find('#');
-		if(pos>=0) fout<<line.substr(0,pos)<<"\n";
-		else fout << line << "\n";
+		for(int i=1;i<=10;i++){
+		    fout<<line<<"\n";
+		}
 	}
 	fin.close();
 	fout.close();
@@ -18,5 +18,5 @@ int main() {
 	string in_name, out_name;
 	cin >> in_name;
 	cin >> out_name;
-	removeComments(in_name, out_name);
+	readWrite(in_name, out_name);
 }
